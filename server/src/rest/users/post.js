4921d578ccd5cbcf85cb4b053c160cdcee02c145
juaamol <jambulud@gmail.com>
2021-11-03 12:13:10 +0100

    create post function that returns users

---
+++ server/src/rest/users/post.js
@@ -0,0 +1,39 @@
+const database = require('../../database/database');
+
+function postUsers(req, res) {
+    const body = req.body;
+    const query = fillEmptyValuesQuery(body.query || {});
+    const users = database.getUsers(query.page, query.sortBy, query.name);
+
+    res.type('application/json')
+        .status('200')
+        .set('Access-Control-Allow-Origin', '*')
+        .set('Access-Control-Allow-Methods', 'POST')
+        .set(
+            'Access-Control-Allow-Headers',
+            'Origin, Content-Type, X-Auth-Token'
+        )
+        .send(JSON.stringify(users))
+}
+
+function fillEmptyValuesQuery(query) {
+    const defaultValues = defaultQuery();
+
+    return {
+        name: query.name || defaultValues.name,
+        page: query.page || defaultValues.page,
+        sortBy: query.sortBy || defaultValues.sortBy,
+    }
+}
+
+function defaultQuery() {
+    return {
+        name: '',
+        page: 0,
+        sortBy: database.SORT_BY.name
+    };
+}
+
+module.exports = {
+    postUsers
+};
