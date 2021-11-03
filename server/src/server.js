46874f181236414c307f1fd8caaed14f78047795
juaamol <jambulud@gmail.com>
2021-11-04 00:40:56 +0100

    server with environment variable

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const options = require('./rest/users/options');
 const post = require('./rest/users/post');
+const env = require('./environment/environment');
 
 const app = express();
 
 app.use(express.json())
 app.options('/users', options.optionsUsers);
 app.post('/users', post.postUsers);
-app.listen('8081')
+app.listen(env.PORT)
 
 exports = module.exports = app;
