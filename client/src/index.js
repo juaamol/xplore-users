1c7482083022cf232a2e0e45be6f8bccff8fd199
juaamol <jambulud@gmail.com>
2021-11-03 23:59:14 +0100

    client: Show more

--- client/src/index.js
+++ client/src/index.js
@@ -3,36 +3,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createAlertComponent } from './components/alert/alert';
 import { createCardsListComponent } from './components/cards-list/cards-list';
 import { createNavbarComponent } from './components/navbar/navbar';
+import { createShowMoreComponent } from './components/show-more/show-more';
 import { createSortComponent } from './components/sort/sort';
 import usersService from './services/users';
 import './style.css';
 
 
 function setNavBar(query) {
-  document.querySelector('#layout-navbar')
-    .appendChild(createNavbarComponent({
+  document.querySelector('#layout-navbar').appendChild(
+    createNavbarComponent({
       onType: (typed) => {
         query.name = typed;
+        query.page = 0;
         setUsers(query);
       }
-    }))
+    })
+  )
 }
 
 function setSortBy(query) {
-  document.querySelector('#layout-sort-by')
-    .appendChild(createSortComponent({
+  document.querySelector('#layout-sort-by').appendChild(
+    createSortComponent({
       onByNameClick: () => {
         query.sortBy = 'name';
+        query.page = 0;
         setUsers(query);
       },
       onByScoreClick: () => {
         query.sortBy = 'score';
+        query.page = 0;
         setUsers(query);
       }
-    }));
+    })
+  );
 }
 
-async function setUsers(query) {
+function setShowMore(query) {
+  document.querySelector('#layout-more').appendChild(
+    createShowMoreComponent({
+      onClick: () => {
+        query.page = query.page + 1;
+        setUsers(query, true);
+      }
+    })
+  );
+}
+
+async function setUsers(query, append = false) {
   const layoutUsersElement = document.querySelector('#layout-users')
   const layoutAlerstElement = document.querySelector('#layout-alerts')
 
@@ -44,8 +61,15 @@ async function setUsers(query) {
     );
 
     if (users?.length) {
-      layoutUsersElement.replaceChildren(createCardsListComponent(users));
-    } else {
+      if (append) {
+        layoutUsersElement.appendChild(createCardsListComponent(users));
+      } else {
+        layoutUsersElement.replaceChildren(createCardsListComponent(users));
+      }
+    }
+
+    if (query.page === 0 && users?.length === 0) {
+      layoutUsersElement.innerHTML = '';
       layoutAlerstElement.replaceChildren(
         createAlertComponent(
           'There are no users that match the criteria',
@@ -56,10 +80,11 @@ async function setUsers(query) {
 
 
   } catch (error) {
+
     layoutAlerstElement.replaceChildren(
       createAlertComponent(
         `Error: ${error}`,
-        'warning'
+        'danger'
       )
     );
   }
@@ -76,6 +101,7 @@ async function setComponents() {
   setNavBar(query);
   setSortBy(query);
   setUsers(query);
+  setShowMore(query);
 }
 
 setComponents();
