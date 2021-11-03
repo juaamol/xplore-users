5d19095a291a1f484cd15bd3d16030b3842b3bfa
juaamol <jambulud@gmail.com>
2021-11-03 15:50:51 +0100

    fix format date from backend

--- server/src/database/users/user.js
+++ server/src/database/users/user.js
@@ -5,10 +5,14 @@ function generateUser(id) {
     return {
         id,
         name: faker.name.findName(),
-        birthday: faker.date.between('1980-01-01', '2010-01-05'),
+        birthday: birthday(faker.date.between('1980-01-01', '2010-01-05')),
         score: faker.datatype.number(9999),
         avatarUrl: `https://robohash.org/${id}`,
     }
+}
+
+function birthday(date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
 module.exports = {
